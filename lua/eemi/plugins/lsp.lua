66034bdd951a32ecaeb195764f7f19ed058996d7
eeemi <eeemula@gmail.com>
2024-08-11 23:26:12 +0300

    Add more plugins

--- lua/eemi/plugins/lsp.lua
+++ lua/eemi/plugins/lsp.lua
@@ -8,13 +8,56 @@ return {
     dependencies = {
         "williamboman/mason.nvim",
         "williamboman/mason-lspconfig.nvim",
+        "hrsh7th/cmp-nvim-lsp",
     },
 
     config = function()
-        require("mason").setup()
-        require("mason-lspconfig").setup({
+        local mason = require("mason")
+        local mason_lspconfig = require("mason-lspconfig")
+        local lspconfig = require("lspconfig")
+        local cmp_nvim_lsp = require("cmp_nvim_lsp")
+
+
+        mason.setup()
+        mason_lspconfig.setup({
             ensure_installed = { "lua_ls", },
         })
+
+        -- ========================================
+        -- lua_ls
+        -- https://github.com/neovim/nvim-lspconfig/blob/master/lua/lspconfig/server_configurations/lua_ls.lua 
+        -- ========================================
+        lspconfig.lua_ls.setup {
+            on_init = function(client)
+                local path = client.workspace_folders[1].name
+                if vim.loop.fs_stat(path..'/.luarc.json') or vim.loop.fs_stat(path..'/.luarc.jsonc') then
+                    return
+                end
+
+                client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+                    runtime = {
+                        -- Tell the language server which version of Lua you're using
+                        -- (most likely LuaJIT in the case of Neovim)
+                        version = 'LuaJIT'
+                    },
+                    -- Make the server aware of Neovim runtime files
+                    workspace = {
+                        checkThirdParty = false,
+                        library = {
+                            vim.env.VIMRUNTIME
+                            -- Depending on the usage, you might want to add additional paths here.
+                            -- "${3rd}/luv/library"
+                            -- "${3rd}/busted/library",
+                        }
+                        -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
+                        -- library = vim.api.nvim_get_runtime_file("", true)
+                    }
+                })
+            end,
+            settings = {
+                Lua = {}
+            }
+        }
 
         print("Hello!!!!!")
 
--- lua/eemi/plugins/telescope.lua
+++ lua/eemi/plugins/telescope.lua
@@ -1,9 +1,28 @@
 return {
     'nvim-telescope/telescope.nvim', tag = '0.1.8',
     -- or                              , branch = '0.1.x',
-    dependencies = { 'nvim-lua/plenary.nvim' },
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    },
 
     config = function()
+        -- https://github.com/nvim-telescope/telescope-fzf-native.nvim?tab=readme-ov-file#telescope-setup-and-configuration 
+        require('telescope').setup {
+            extensions = {
+                fzf = {
+                    fuzzy = true,                    -- false will only do exact matching
+                    override_generic_sorter = true,  -- override the generic sorter
+                    override_file_sorter = true,     -- override the file sorter
+                    case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+                    -- the default case_mode is "smart_case"
+                }
+            }
+        }
+        -- To get fzf loaded and working with telescope, you need to call
+        -- load_extension, somewhere after setup function:
+        require('telescope').load_extension('fzf')
+
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = '[F]ind files'})
         vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Live [G]rep' })
