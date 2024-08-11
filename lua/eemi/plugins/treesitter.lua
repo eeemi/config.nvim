11ce8ac28520cef7bb1e11c44f661b45d811d7b1
eeemi <eeemula@gmail.com>
2024-08-11 23:26:12 +0300

    Add more plugins

--- lua/eemi/plugins/treesitter.lua
+++ lua/eemi/plugins/treesitter.lua
@@ -1,7 +1,7 @@
 return {
     "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
-        config = function () 
+        config = function ()
             local configs = require("nvim-treesitter.configs")
 
             configs.setup({
@@ -9,7 +9,7 @@ return {
                     ensure_installed = { "lua" },
                     sync_install = false,
                     highlight = { enable = true },
-                    indent = { enable = true },  
+                    indent = { enable = true },
                     })
     end
 }
