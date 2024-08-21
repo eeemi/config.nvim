27d1d111a2303935c0369ff3689e9d5223f7e862
eeemi <eeemula@gmail.com>
2024-08-21 19:17:10 +0300

    Add functions to debugger

--- lua/eemi/plugins/nvim-dap.lua
+++ lua/eemi/plugins/nvim-dap.lua
@@ -10,7 +10,15 @@ return {
         local ui = require("dapui")
         local dap_go = require("dap-go")
 
-        vim.keymap.set("n", "<space>b", dap.toggle_breakpoint)
+        vim.keymap.set("n", "<space>b", dap.toggle_breakpoint, { desc = "Toggle breakpoint"})
+
+        vim.keymap.set("n", "<F1>", dap.continue)
+        vim.keymap.set("n", "<F2>", dap.step_into)
+        vim.keymap.set("n", "<F3>", dap.step_over)
+        vim.keymap.set("n", "<F4>", dap.step_out)
+        vim.keymap.set("n", "<F5>", dap.step_back)
+        vim.keymap.set("n", "<F13>", dap.restart)
+
 
         ui.setup()
         dap_go.setup ({
@@ -66,6 +74,20 @@ return {
                 verbose = false,
             },
         })
+
+        -- open ui
+        dap.listeners.before.attach.dapui_config = function()
+            ui.open()
+        end
+        dap.listeners.before.launch.dapui_config = function()
+            ui.open()
+        end
+        dap.listeners.before.event_terminated.dapui_config = function()
+            ui.close()
+        end
+        dap.listeners.before.event_exited.dapui_config = function()
+            ui.close()
+        end
     end,
 }
 
