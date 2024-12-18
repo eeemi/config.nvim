5bc6aad09e1fd83783b457d7bf7e548c04f31f07
eeemi <eeemula@gmail.com>
2024-12-18 11:03:42 +0200

    style: make which key descriptions clearer

--- lua/eemi/plugins/telescope.lua
+++ lua/eemi/plugins/telescope.lua
@@ -45,10 +45,10 @@ config = function()
     require("telescope").load_extension("ui-select")
 
     local builtin = require('telescope.builtin')
-    vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find [F]iles' })
-    vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live [G]rep' })
-    vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope [B]uffers' })
-    vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope [H]elp tags' })
+    vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find [f]iles' })
+    vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live [g]rep' })
+    vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope [b]uffers' })
+    vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope [h]elp tags' })
 
     end,
 }
