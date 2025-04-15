b6efd6e8e44efdd43c4c038612f58454a330bafb
eeemi <eeemula@gmail.com>
2025-04-15 17:13:45 +0300

    feat(dap): dap debugging configuration + dap-go

--- lua/eemi/plugins/dap.lua
+++ lua/eemi/plugins/dap.lua
@@ -1,22 +1,45 @@
 return {
     "mfussenegger/nvim-dap",
-
     dependencies = {
         "rcarriga/nvim-dap-ui",
+        { "theHamsta/nvim-dap-virtual-text",
+        dependencies = { "nvim-treesitter/nvim-treesitter" }, -- NOTE: which loads first, this dep or the one in its own file?
+        },
         "nvim-neotest/nvim-nio",
+        { "jay-babu/mason-nvim-dap.nvim",
+        dependencies = { "williamboman/mason.nvim", }, -- NOTE: which loads first, this dep or the one in its own file?
+        },
         "leoluz/nvim-dap-go", -- go
     },
 
     config = function ()
+        local dap = require("dap")
+        local dapui = require("dapui")
+        local dapui_widgets = require('dap.ui.widgets')
+        local nvim_dap_virtual_text = require("nvim-dap-virtual-text")
+        local nio = require("nio")
+        local mason_nvim_dap = require("mason-nvim-dap")
+
         -- ----------------------------------------------------------------
         -- dap/ui setup
         -- ----------------------------------------------------------------
 
-        local dap = require("dap")
-        local dapui = require("dapui")
-        local dapui_widgets = require('dap.ui.widgets')
-
-        dapui.setup()
+        -- dapui.setup()
+        dapui.setup({
+            controls = {
+                icons = {
+                    disconnect = " (C-F5)",
+                    pause = "",
+                    play = " (F5)",
+                    run_last = " (F9)",
+                    step_back = " (F10)",
+                    step_into = " (F8)",
+                    step_out = " (F6)",
+                    step_over = " (F7)",
+                    terminate = " (S-F5)"
+                }
+            },
+        })
 
         dap.listeners.before.attach.dapui_config = function()
             dapui.open()
@@ -31,49 +54,117 @@ return {
             dapui.close()
         end
 
-        vim.keymap.set('n', '<F5>', function() dap.continue() end)
-        vim.keymap.set('n', '<F6>', function() dap.terminate() end)
-        vim.keymap.set('n', '<F10>', function() dap.step_over() end)
-        vim.keymap.set('n', '<F11>', function() dap.step_into() end)
-        vim.keymap.set('n', '<F12>', function() dap.step_out() end)
-        vim.keymap.set('n', '<Leader>b', function() dap.toggle_breakpoint() end, {desc = 'Toggle [b]reakpoint'})
-        vim.keymap.set('n', '<Leader>DB', function() dap.set_breakpoint() end, {desc = 'Set [B]reakpoint'})
-        vim.keymap.set('n', '<Leader>Dp', function() dap.set_breakpoint(nil, nil, vim.fn.input('Log point message: ')) end)
-        vim.keymap.set('n', '<Leader>Dr', function() dap.repl.open() end, {desc = 'Open [R]epl'})
-        -- vim.keymap.set('n', '<Leader>dl', function() dap.run_last() end)
-        vim.keymap.set({'n', 'v'}, '<Leader>Dh', function() dapui_widgets.hover() end, {desc = '[H]over widgets'})
-        vim.keymap.set({'n', 'v'}, '<Leader>Dp', function() dapui_widgets.preview() end, {desc = '[P]review widgets'})
-        vim.keymap.set('n', '<Leader>Df', function() local widgets = dapui_widgets widgets.centered_float(widgets.frames) end,
+        -- from `:h dap-mappings`
+        vim.keymap.set({'n', 'v'}, "<leader>De", function() require("dapui").eval() end, {desc = "[e]val virtual text"})
+        vim.keymap.set('n', '<leader>Dt', function() dapui.toggle({}) end, {desc = "[t]oggle UI"})
+        vim.keymap.set('n', '<leader>Ds', function() dap.continue() end, {desc = "[S]tart (F5)"})
+        vim.keymap.set('n', '<F5>', function() dap.continue() end, {desc = "Start (F5)"})
+        vim.keymap.set('n', '<F17>', function() dap.terminate() end, {desc = "Terminate <S-F5>"})
+        vim.keymap.set('n', '<F9>', function() dap.run_last() end)
+        vim.keymap.set('n', '<F7>', function() dap.step_over() end)
+        vim.keymap.set('n', '<F8>', function() dap.step_into() end)
+        vim.keymap.set('n', '<F6>', function() dap.step_out() end)
+        vim.keymap.set('n', '<F10>', function() dap.step_back() end) -- is this needed?
+        vim.keymap.set('n', '<F29>', function() dap.disconnect() end, {desc = "Disconnect (Ctrl+F5)"}) -- is this needed?
+        -- vim.keymap.set('n', '<F41>', function() dap.restart() end, {desc = "Restart (C-S-F5)"}) -- is this needed?
+        vim.keymap.set('n', '<leader>b', function() dap.toggle_breakpoint() end, {desc = 'Toggle [b]reakpoint'})
+        vim.keymap.set('n', '<leader>DB', function() dap.set_breakpoint() end, {desc = 'Set [B]reakpoint'})
+        vim.keymap.set('n', '<leader>Dp', function() dap.set_breakpoint(nil, nil, vim.fn.input('Log point message: ')) end)
+        vim.keymap.set('n', '<leader>Dr', function() dap.repl.open() end, {desc = 'Open [R]epl'})
+        -- vim.keymap.set('n', '<leader>Dl', function() dap.run_last() end)
+        vim.keymap.set({'n', 'v'}, '<leader>Dh', function() dapui_widgets.hover() end, {desc = '[H]over widgets'})
+        vim.keymap.set({'n', 'v'}, '<leader>Dp', function() dapui_widgets.preview() end, {desc = '[P]review widgets'})
+        vim.keymap.set('n', '<leader>Df', function() local widgets = dapui_widgets widgets.centered_float(widgets.frames) end,
         {desc = 'Centered float (frames)'})
-        vim.keymap.set('n', '<Leader>Ds', function() local widgets = dapui_widgets widgets.centered_float(widgets.scopes) end,
+        vim.keymap.set('n', '<leader>Ds', function() local widgets = dapui_widgets widgets.centered_float(widgets.scopes) end,
         {desc = 'Centered float (scopes)'})
 
-        vim.fn.sign_define('DapBreakpoint',
-        {
-            text='🔴',
-            texthl='DapBreakpointSymbol',
-            linehl='DapBreakpoint',
-            numhl='DapBreakpoint'
-        })
-        vim.fn.sign_define('DapStopped',
-        {
-            text='>',
-            texthl='DapStoppedSymbol',
-            linehl='DapBreakpoint',
-            numhl='DapBreakpoint'
+        -- from (here)[https://www.lazyvim.org/extras/dap/core]. Same [here](https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/plugins/extras/dap/core.lua)
+        vim.api.nvim_set_hl(0, "DapStoppedLine", { default = true, link = "Visual" })
+        local dap_icons = {
+            Stopped             = { "", "DiagnosticWarn", "DapStoppedLine" },
+            Breakpoint          = { "", "DiagnosticError"}, -- this makes a small red circle
+            BreakpointCondition = "",
+            BreakpointRejected  = { "", "DiagnosticError" },
+            LogPoint            = ".>",
+        }
+        for name, sign in pairs(dap_icons) do
+            sign = type(sign) == "table" and sign or { sign }
+            vim.fn.sign_define(
+                "Dap" .. name,
+                { text = sign[1], texthl = sign[2] or "DiagnosticInfo", linehl = sign[3], numhl = sign[3] }
+            )
+        end
+
+        -- NOTE: this does essentially the same as above
+        -- vim.fn.sign_define('DapBreakpoint',
+        -- {
+        --     text= '',
+        --     texthl='DiagnosticError',
+        -- })
+        -- vim.fn.sign_define('DapStopped',
+        -- {
+        --     text='',
+        --     texthl='DiagnosticWarn',
+        --     linehl='DapStoppedLine',
+        --     numhl='DapStoppedLine'
+        -- })
+
+        -- ----------------------------------------------------------------
+        -- nvim_dap_virtual_text setup
+        -- ----------------------------------------------------------------
+
+        -- from https://github.com/theHamsta/nvim-dap-virtual-text
+        nvim_dap_virtual_text.setup({
+            enabled = true,                        -- enable this plugin (the default)
+            enabled_commands = true,               -- create commands DapVirtualTextEnable, DapVirtualTextDisable, DapVirtualTextToggle, (DapVirtualTextForceRefresh for refreshing when debug adapter did not notify its termination)
+            highlight_changed_variables = true,    -- highlight changed values with NvimDapVirtualTextChanged, else always NvimDapVirtualText
+            highlight_new_as_changed = false,      -- highlight new variables in the same way as changed variables (if highlight_changed_variables)
+            show_stop_reason = true,               -- show stop reason when stopped for exceptions
+            commented = false,                     -- prefix virtual text with comment string
+            only_first_definition = true,          -- only show virtual text at first definition (if there are multiple)
+            all_references = false,                -- show virtual text on all all references of the variable (not only definitions)
+            clear_on_continue = false,             -- clear virtual text on "continue" (might cause flickering when stepping)
+            --- A callback that determines how a variable is displayed or whether it should be omitted
+            --- @param variable Variable https://microsoft.github.io/debug-adapter-protocol/specification#Types_Variable
+            --- @param buf number
+            --- @param stackframe dap.StackFrame https://microsoft.github.io/debug-adapter-protocol/specification#Types_StackFrame
+            --- @param node userdata tree-sitter node identified as variable definition of reference (see `:h tsnode`)
+            --- @param options nvim_dap_virtual_text_options Current options for nvim-dap-virtual-text
+            --- @return string|nil A text how the virtual text should be displayed or nil, if this variable shouldn't be displayed
+            display_callback = function(variable, buf, stackframe, node, options)
+                -- by default, strip out new line characters
+                if options.virt_text_pos == 'inline' then
+                    return ' = ' .. variable.value:gsub("%s+", " ")
+                else
+                    return variable.name .. ' = ' .. variable.value:gsub("%s+", " ")
+                end
+            end,
+            -- position of virtual text, see `:h nvim_buf_set_extmark()`, default tries to inline the virtual text. Use 'eol' to set to end of line
+            virt_text_pos = vim.fn.has 'nvim-0.10' == 1 and 'inline' or 'eol',
+
+            -- experimental features:
+            all_frames = false,                    -- show virtual text for all stack frames not only current. Only works for debugpy on my machine.
+            virt_lines = false,                    -- show virtual lines instead of virtual text (will flicker!)
+            virt_text_win_col = nil                -- position the virtual text at a fixed window column (starting from the first text column) ,
+            -- e.g. 80 to position at column 80, see `:h nvim_buf_set_extmark()`
         })
 
-        -- local set_namespace = vim.api.nvim__set_hl_ns or vim.api.nvim_set_hl_ns
-        -- local namespace = vim.api.nvim_create_namespace("dap-hlng")
-        -- vim.api.nvim_set_hl(namespace, 'DapBreakpoint', { fg='#eaeaeb', bg='#ffffff' })
-        -- vim.api.nvim_set_hl(namespace, 'DapLogPoint', { fg='#eaeaeb', bg='#ffffff' })
-        -- vim.api.nvim_set_hl(namespace, 'DapStopped', { fg='#eaeaeb', bg='#ffffff' })
-        --
-        -- vim.fn.sign_define('DapBreakpoint', { text='', texthl='DapBreakpoint', linehl='DapBreakpoint', numhl='DapBreakpoint' })
-        -- vim.fn.sign_define('DapBreakpointCondition', { text='ﳁ', texthl='DapBreakpoint', linehl='DapBreakpoint', numhl='DapBreakpoint' })
-        -- vim.fn.sign_define('DapBreakpointRejected', { text='', texthl='DapBreakpoint', linehl='DapBreakpoint', numhl= 'DapBreakpoint' })
-        -- vim.fn.sign_define('DapLogPoint', { text='', texthl='DapLogPoint', linehl='DapLogPoint', numhl= 'DapLogPoint' })
-        -- vim.fn.sign_define('DapStopped', { text='', texthl='DapStopped', linehl='DapStopped', numhl= 'DapStopped' })
+        -- ----------------------------------------------------------------
+        -- mason_nvim_dap setup
+        -- ----------------------------------------------------------------
+
+        -- from https://github.com/jay-babu/mason-nvim-dap.nvim#default-configuration 
+        mason_nvim_dap.setup({
+            -- A list of adapters to install if they're not already installed.
+            -- This setting has no relation with the `automatic_installation` setting.
+            -- ensure_installed = {},
+            ensure_installed = {
+                'delve',
+                -- 'debugpy',
+                -- '', -- some cpp debugger?
+            },
+        })
 
         -- ----------------------------------------------------------------
         -- dap go
