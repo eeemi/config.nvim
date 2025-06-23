c8c5d4790d21c734ee521739a721ec5a604c6642
eeemi <eeemula@gmail.com>
2025-06-23 18:51:58 +0300

    feat(iron): keymap & small config changes

--- lua/eemi/plugins/iron.lua
+++ lua/eemi/plugins/iron.lua
@@ -15,10 +15,12 @@ return {
                     sh = {
                         -- Can be a table or a function that
                         -- returns a table (see below)
-                        command = {"zsh"}
+                        -- command = {"zsh"}
+                        command = {"bash"}
                     },
                     python = {
-                        command = { "python3" },  -- or { "ipython", "--no-autoindent" }
+                        -- command = { "python3" },  -- or { "ipython", "--no-autoindent" }
+                        command = { "ipython", "--no-autoindent" },
                         format = common.bracketed_paste_python,
                         block_dividers = { "# %%", "#%%" },
                     }
@@ -33,7 +35,10 @@ return {
                 end,
                 -- How the repl window will be displayed
                 -- See below for more information
-                repl_open_cmd = view.bottom(40),
+                -- repl_open_cmd = view.bottom(40),
+                -- repl_open_cmd = view.right("%40"),
+                -- repl_open_cmd = view.split("%40"),
+                repl_open_cmd = view.split.vertical.botright("%40")
 
                 -- repl_open_cmd can also be an array-style table so that multiple 
                 -- repl_open_commands can be given.
@@ -53,28 +58,32 @@ return {
                 -- Iron doesn't set keymaps by default anymore.
                 -- You can set them here or manually add keymaps to the functions in iron.core
                 keymaps = {
-                    toggle_repl = "<space>rr", -- toggles the repl open and closed.
+
+                    toggle_repl = "<space>ii", -- toggles the repl open and closed.
+
                     -- If repl_open_command is a table as above, then the following keymaps are
                     -- available
                     -- toggle_repl_with_cmd_1 = "<space>rv",
                     -- toggle_repl_with_cmd_2 = "<space>rh",
-                    restart_repl = "<space>rR", -- calls `IronRestart` to restart the repl
-                    send_motion = "<space>sc",
-                    visual_send = "<space>sc",
-                    send_file = "<space>sf",
-                    send_line = "<space>sl",
-                    send_paragraph = "<space>sp",
-                    send_until_cursor = "<space>su",
-                    send_mark = "<space>sm",
-                    send_code_block = "<space>sb",
-                    send_code_block_and_move = "<space>sn",
-                    mark_motion = "<space>mc",
-                    mark_visual = "<space>mc",
-                    remove_mark = "<space>md",
-                    cr = "<space>s<cr>",
-                    interrupt = "<space>s<space>",
-                    exit = "<space>sq",
-                    clear = "<space>cl",
+
+                    restart_repl = "<space>iR", -- calls `IronRestart` to restart the repl
+                    send_motion = "<space>isc",
+                    visual_send = "<space>isc",
+                    send_file = "<space>isf",
+                    send_line = "<space>isl",
+                    send_paragraph = "<space>isp",
+                    send_until_cursor = "<space>isu",
+                    send_mark = "<space>ism",
+                    send_code_block = "<space>isb",
+                    send_code_block_and_move = "<space>isn",
+                    -- mark_motion = "<space>mc",
+                    -- mark_visual = "<space>mc",
+                    -- remove_mark = "<space>md",
+                    cr = "<space>is<cr>",
+                    interrupt = "<space>is<space>",
+                    exit = "<space>isq",
+                    clear = "<space>il",
+
                 },
                 -- If the highlight is on, you can change how it looks
                 -- For the available options, check nvim_set_hl
@@ -85,8 +94,8 @@ return {
             }
 
             -- iron also has a list of commands, see :h iron-commands for all available commands
-            vim.keymap.set('n', '<space>rf', '<cmd>IronFocus<cr>')
-            vim.keymap.set('n', '<space>rh', '<cmd>IronHide<cr>')
+            vim.keymap.set('n', '<space>irf', '<cmd>IronFocus<cr>')
+            vim.keymap.set('n', '<space>irh', '<cmd>IronHide<cr>')
 
         end,
 }
