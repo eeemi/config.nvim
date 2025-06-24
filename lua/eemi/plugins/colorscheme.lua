c4a55bcbfa29de25ebafe3fc44d38eeb283fc498
eeemi <eeemula@gmail.com>
2025-06-24 19:04:15 +0300

    feat: vertical window separator line

--- lua/eemi/plugins/colorscheme.lua
+++ lua/eemi/plugins/colorscheme.lua
@@ -3,6 +3,13 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
+
+        require("tokyonight").setup({
+          on_colors = function(colors)
+            colors.border = "#565f89"
+          end
+        })
+
         vim.cmd([[colorscheme tokyonight-night]])
     end,
 }
