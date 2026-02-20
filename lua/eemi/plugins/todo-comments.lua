f87e43cc98afd57e7037552e1bf5b480795aa537
eeemi <eeemula@gmail.com>
2026-02-20 21:55:13 +0200

    feat(todo-comments): jump keymaps for critical todo-comments

--- lua/eemi/plugins/todo-comments.lua
+++ lua/eemi/plugins/todo-comments.lua
@@ -8,10 +8,16 @@ return {
 
         vim.keymap.set("n", "]t", function()
             require("todo-comments").jump_next()
-        end, { desc = "Next [t]odo comment" }),
+        end, { desc = "Next [t]odo comment (all)" }),
+        vim.keymap.set("n", "]T", function()
+            require("todo-comments").jump_next({keywords = {"TODO","FIX","FIXME","BUG","FIXIT","ISSUE"}})
+        end, { desc = "Next [T]odo comment" }),
         vim.keymap.set("n", "[t", function()
             require("todo-comments").jump_prev()
         end, { desc = "Previous [t]odo comment" })
+        vim.keymap.set("n", "]T", function()
+            require("todo-comments").jump_prev({keywords = {"TODO","FIX","FIXME","BUG","FIXIT","ISSUE"}})
+        end, { desc = "Previous [T]odo comment (all)" })
 
     },
 }
