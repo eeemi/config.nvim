080ccb6333ef6cace90ebaa6d9af227be091ec9f
eeemi <eeemula@gmail.com>
2026-02-23 08:59:03 +0200

    fix(todo-comments): missing symbol

--- lua/eemi/plugins/todo-comments.lua
+++ lua/eemi/plugins/todo-comments.lua
@@ -14,7 +14,7 @@ return {
         end, { desc = "Next [T]odo comment" }),
         vim.keymap.set("n", "[t", function()
             require("todo-comments").jump_prev()
-        end, { desc = "Previous [t]odo comment" })
+        end, { desc = "Previous [t]odo comment" }),
         vim.keymap.set("n", "]T", function()
             require("todo-comments").jump_prev({keywords = {"TODO","FIX","FIXME","BUG","FIXIT","ISSUE"}})
         end, { desc = "Previous [T]odo comment (all)" })
